35c99ef02e2eab82d02fd3553ee0353fb7efc65d
Luis Manzo <manzosk3@gmail.com>
2017-07-26 14:32:56 -0700

    uses promise pattern to verify POST or PUT request was made to server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
 const Drone = require('./model/drone.js');
+const parseBody = require('./lib/parse_body.js');
 
-const server = http.createServer()
+const server = http.createServer(function (req) {
+  parseBody(req)
+    .then(function (msg) {
+      console.log(msg)
+    })
+    .catch(function (err) {
+      console.error(err)
+    });
+});
 
-const phantom = new Drone('phantom', 4);
-console.log(phantom);
-
-server.listen(PORT, function() {
+server.listen(PORT, function () {
   console.log('server running on ', PORT);
-})
+});
