762b407a41d8d94abdefc4663e3a220e406e8cfb
Luis Manzo <manzosk3@gmail.com>
2017-07-26 13:07:47 -0700

    creates an object constructor with a simple resource and 3 properties

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
+const Drone = require('./model/drone.js');
 
 const server = http.createServer()
 
+const phantom = new Drone('phantom', 4);
+console.log(phantom);
 
 server.listen(PORT, function() {
   console.log('server running on ', PORT);
