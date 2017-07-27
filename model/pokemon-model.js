0a4c7f91187bcdaeae040a0617a57abb97d52720
Loaye <jr.carstens00@gmail.com>
2017-07-26 17:16:56 -0700

    finishing up

--- model/pokemon-model.js
+++ model/pokemon-model.js
@@ -2,13 +2,13 @@
 
 const uuidv4 = require('uuid/v4');
 
-module.exports = function(pokemon, type, gen) {
-  if(!pokemon)throw new Error('expect Pokemon name');
+module.exports = function(name, type, gen) {
+  if(!name)throw new Error('expect Pokemon name');
   if(!type)throw new Error('expected Pokemon type');
   if(!gen)throw new Error('expected a generation')
 
   this.id = uuidv4();
-  this.pokemon = pokemon;
+  this.name = name;
   this.type = type;
   this.gen = gen;
 };
