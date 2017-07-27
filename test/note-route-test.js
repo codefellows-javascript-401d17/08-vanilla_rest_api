3538fc67a008d543aaa3f239321a51619f9ff658
ohjonah <ohjonah@gmail.com>
2017-07-26 21:57:08 -0700

    Fixes typo in expect block

--- test/note-route-test.js
+++ test/note-route-test.js
@@ -14,7 +14,7 @@ describe('Note Routes', function() {
       .send({ name: 'test name', content: 'test content'})
       .end((err, res) => {
         if (err) return done(err);
-        expect(res.status.to.equal(200));
+        expect(res.status).to.equal(200);
         expect(res.body.name).to.equal('test name');
         expect(res.body.content).to.equal('test content');
 
